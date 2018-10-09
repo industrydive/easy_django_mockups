1c2ad8d0a8fce2eb55700b500898e2ddf35f45b1
Josh Brown <jbrown@industrydive.com>
2018-10-09 13:09:05 -0400

    add some almost working test stuff

--- easymockups/tests/test_mockups.py
+++ easymockups/tests/test_mockups.py
@@ -1,10 +1,24 @@
-from unittest import TestCase
+#from unittest import TestCase
+import django
 from django.conf import settings
+from django.test import TestCase, Client
+from easymockups.views import display_template
+import test_settings
+from django.urls import reverse
 
+#if not settings.configured:
+#    settings.configure(default_settings=test_settings, DEBUG=True)
+django.setup()
 #settings.configure()
 
-
 class TestJoke(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+
+
     def test_is_string(self):
         s = 'hi'
-        self.assertTrue(True)
+
+        getreq = self.client.get(reverse('display_template', kwargs={'mockup_template_name': 'test.html'}))
+        self.assertEqual(200, getreq.status_code)
