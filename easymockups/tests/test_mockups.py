98073d7d72bdaca11b32dc9860b4ca5990b7d1a5
Josh Brown <josh.s.brown@hotmail.com>
2018-10-09 23:18:23 -0400

    move test strucgture around

--- easymockups/tests/test_mockups.py
+++ easymockups/tests/test_mockups.py
@@ -14,28 +14,27 @@ django.setup()
 
 class TestFileCreation(TestCase):
     def setUp(self):
-        pass
+        self.filename_base='testsuitefile'
+        self.html_file_path = os.path.join('easymockups', settings.MOCKUPS_DIR, '{}.html'.format(self.filename_base))
+        self.json_file_path = os.path.join('easymockups', settings.MOCKUPS_DIR, '{}.json'.format(self.filename_base))
+        self.testsuite_urlpath = reverse('display_template', kwargs={'mockup_template_name': 'testsuitefile.html'})
 
     def test_create_file(self):
-        filename_base='testsuitefile'
-        html_file_path = os.path.join('easymockups', settings.MOCKUPS_DIR, '{}.html'.format(filename_base))
-        json_file_path = os.path.join('easymockups', settings.MOCKUPS_DIR, '{}.json'.format(filename_base))
-        testsuite_urlpath = reverse('display_template', kwargs={'mockup_template_name': 'testsuitefile.html'})
 
 
-        with open(html_file_path, 'w') as f:
+        with open(self.html_file_path, 'w') as f:
             html_contents = """
                 <html><body>We should see the output from "testvar" in the json file 
                 below after the exclamation points!! {{ testvar }} </body></html>
                 """
             f.write(html_contents)
 
-        with open(json_file_path, 'w') as f:
+        with open(self.json_file_path, 'w') as f:
             json.dump({"testvar": "this is some test json!"}, f)
 
 
         # Test the template renderinng works with the json file we created in the above lines
-        resp = self.client.get(testsuite_urlpath)
+        resp = self.client.get(self.testsuite_urlpath)
         self.assertEqual(200, resp.status_code)
         self.assertIn('this is some test json!', resp.content.decode("utf-8"))
 
@@ -43,14 +42,14 @@ class TestFileCreation(TestCase):
         # Test taht even though we remove the json file, we sill get a valid reesponse bc the HTML file still exists
         # We only test for a substring at the end of the html_contents string because the django renderer
         # should have stripped out the {{ testvar }} part during renderinng, since the json object doesnt exist any more
-        os.remove(json_file_path)
-        resp = self.client.get(testsuite_urlpath)
+        os.remove(self.json_file_path)
+        resp = self.client.get(self.testsuite_urlpath)
         self.assertEqual(200, resp.status_code)
         self.assertIn('exclamation points!!  </body', resp.content.decode('utf-8'))
 
 
         # Test that removing the HTML file will cause the page to 404
-        os.remove(html_file_path)
-        resp = self.client.get(testsuite_urlpath)
+        os.remove(self.html_file_path)
+        resp = self.client.get(self.testsuite_urlpath)
         self.assertEqual(404, resp.status_code)
 
