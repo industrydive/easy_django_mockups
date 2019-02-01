3f0e8d43f6aa9c500ee572d0f498d75024b1cde9
Josh Brown <jbrown@industrydive.com>
2019-02-01 13:16:33 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-print('\n\n\n find_packages is ', find_packages())
-
 setup(
     name='easymockups',
     version='0.1',
@@ -18,20 +16,18 @@ setup(
     description='A simple Django app to allow for quick protyping of templates without a need for a corresponding Django View.',
     long_description=README,
     url='https://www.example.com/',
-    author='Your Name',
-    author_email='yourname@example.com',
+    author='Industry Dive',
+    author_email='tech.team@industrydive.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
+        'Framework :: Django :: 1.11',  # replace "X.Y" as appropriate
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',  # example license
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         # Replace these appropriately if you are stuck on Python 2.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 2.7x',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
